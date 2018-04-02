de3f90e862edbfb06c46cc50e8940978cf26099b
mhortillano <mhortillano@github.com>
2018-04-02 09:17:40 -1000

    Initial Commit

---
+++ HackWichEight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackWichEight
+//
+//  Created by Mariah Hortillano on 4/2/18.
+//  Copyright © 2018 Mariah Hortillano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
